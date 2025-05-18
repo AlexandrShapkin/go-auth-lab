71bcd749846e43f9bbded426e5616f7e877fa22c
Alexandr Shapkin <shapkin.alexandr.sergeevich@gmail.com>
2025-05-18 16:38:56 +0700

    feat:http server & no auth handler

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,21 +1,60 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"log/slog"
+	"net/http"
 	"os"
+	"os/signal"
 )
 
 const (
+	Addr = ":8080"
+
 	HTTPBasicMode = "http_basic_auth"
 	CookieMode    = "cookie_auth"
 	JWTMode       = "jwt_auth"
+	NoAuthMode    = "no_auth"
 )
 
+type NoAuth struct{}
+
+func (na *NoAuth) LoginHandler() http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("No authorization required! No return value"))
+	})
+}
+
+func (na *NoAuth) ProtectedHandler() http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Access successfully granted!"))
+	})
+}
+
+func NewNoAuth() Auth {
+	return &NoAuth{}
+}
+
+type Auth interface {
+	LoginHandler() http.HandlerFunc
+	ProtectedHandler() http.HandlerFunc
+}
+
+func loggingMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		slog.Info("Request", slog.String("method", r.Method), slog.String("path", r.URL.Path))
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
-	mode := flag.String("mode", HTTPBasicMode, "Режим работы")
+	mode := flag.String("mode", NoAuthMode, "Режим работы")
 
 	flag.Parse()
+
+	var auth Auth
 
 	switch *mode {
 	case HTTPBasicMode:
@@ -25,7 +64,33 @@ func main() {
 	case JWTMode:
 		fmt.Println("JWT Mode") // TODO: to implement
 	default:
-		fmt.Println("Wrong mode")
-		os.Exit(1)
+		slog.Info("Unknown mode, falling back to NoAuth")
+		auth = NewNoAuth()
+	}
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/login", loggingMiddleware(auth.LoginHandler()))
+	mux.HandleFunc("/protected", loggingMiddleware(auth.ProtectedHandler()))
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	server := &http.Server{
+		Addr:    Addr,
+		Handler: mux,
+	}
+
+	go func() {
+		<-ctx.Done()
+		slog.Info("Shutting down...")
+		if err := server.Shutdown(context.Background()); err != nil {
+			slog.Error("Server shutdown error", slog.String("error", err.Error()))
+		}
+	}()
+
+	slog.Info("Starting server", slog.String("addr", Addr), slog.String("mode", *mode))
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		slog.Error("Server error", slog.String("error", err.Error()))
 	}
 }
